460e7abad0389a06a001b0da404ff894571a7a1c
ZigGreen <krisstal.mail@gmail.com>
2019-03-22 18:34:04 +0300

    13213

--- src/Chat.js
+++ src/Chat.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-  state = { msgs: []};
+  state = { msgs: [] };
   componentDidMount() {
-    const socket = global.io('localhost:3001');
-    //var socket = io('damp-caverns-25183.herokuapp.com');
+    //const socket = global.io('localhost:3001');
+    var socket = io("damp-caverns-25183.herokuapp.com");
 
-    const showMessage = msg => this.setState({ msgs: [msg, ...this.state.msgs] });
+    const showMessage = msg =>
+      this.setState({ msgs: [msg, ...this.state.msgs] });
 
-    socket.on('chat message', showMessage);
-    socket.on('chat messages', msg => msg.map(showMessage));
+    socket.on("chat message", showMessage);
+    socket.on("chat messages", msg => msg.map(showMessage));
   }
 
   render() {
     return (
-      <div >
+      <div>
         {this.state.msgs.map(x => (
           <div>{x.text}</div>
         ))}
